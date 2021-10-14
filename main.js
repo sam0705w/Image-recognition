86e3f9d0a37e5acb28bd1e8d06c2a69d005764ef
sam0705w <79701050+sam0705w@users.noreply.github.com>
2021-10-14 17:23:37 +0530

    Update main.js

--- main.js
+++ main.js
@@ -8,10 +8,25 @@ var camera= document.getElementById("camera")
 Webcam.attach("#camera")
 function takesnap(){
     Webcam.snap(function(data_uri){
-        document.getElementById("result").innerHTML="<img src='"+data_uri+"'>"
+        document.getElementById("result").innerHTML="<img id='res' src='"+data_uri+"'>"
     })
 }
 console.log(ml5.version)
 var classifier=ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/U-fRFfBMC/model.json",modelLoaded)
 function modelLoaded(){
-console.log("modelLoaded")}
+console.log("modelLoaded")
+}
+function identify(){
+var img= document.getElementById("res")
+classifier.classify(img,gotresult)
+}
+function gotresult(error,results){
+if(error){
+    console.log(error)
+}
+else{
+    console.log(results)
+    document.getElementById("obgres").innerHTML=results[0].label
+    document.getElementById("accu").innerHTML=results[0].confidence.toFixed(3)
+}
+}
